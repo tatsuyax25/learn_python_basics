dc833fa887e6842b5233acd8893261104796e779
Miguel Urena <urena_miguel82@outlook.com>
2024-02-22 14:13:10 -0800

    Create Data Types

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
 print("Hello, World!")
+
+# String Example
+name = "Miguel"
+# Integer Example
+age = 41
+# Float Example
+height = 1.77
+# Boolean Example
+is_student = False
+# Print variable values
+print(name)
+print(age)
+print(height)
+print(is_student)
