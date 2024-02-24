8252acb2db99e9ff92e387842a2b66d7455ff6b4
Miguel Urena <urena_miguel82@outlook.com>
2024-02-24 13:14:05 -0800

    Conditional Statements

--- project.py
+++ project.py
@@ -54,3 +54,12 @@ is_adult = age >= 18
 is_teenager = age >= 13 and age < 18
 print("Is Adult:", is_adult)
 print("Is Teenager:", is_teenager)
+
+# Conditional Statements
+age = 20
+if age < 18:
+  print("You are a minor.")
+elif 18 <= age < 21:
+  print("You are an adult, but not yet allowed to drink.")
+else:
+  print("You are a legal adult.");
