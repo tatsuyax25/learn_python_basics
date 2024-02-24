6fe813fc160f08cdf6b87bfed9dfe3f4a43a5336
Miguel Urena <urena_miguel82@outlook.com>
2024-02-24 13:51:29 -0800

    Done Tutorial

--- project.py
+++ project.py
@@ -90,3 +90,20 @@ for i in range(5):
     print(f"Skipped iteration with 'continue' at i={i}")
     continue
   print(i)
+
+# Function
+def greet():
+  print("Hello, World!")
+# Call the function to execute
+greet()
+
+def check_age(age):
+  if age < 18:
+    print("You are a minor.")
+  elif 18 <= age < 21:
+    print("You are an adult, but not yet allowed to drink.")
+  else:
+    print("You are a legal adult.")
+
+# Call the function with a specific age
+check_age(20)
